7bf1d3132ab78ed139b0196b0332f0b7b2a2adf3
David <david.garcia@lumation.co>
2021-03-04 12:33:45 -0600

    Se agregaron los paquetes y la estructura de los repositorios

---
+++ src/models/user.model.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = Schema({
+    user: String,
+    password: String,
+    rol: String
+});
+
+module.exports = mongoose.model('users', UserSchema);
