7b9d120db69fe715eae73d3885f986c9843e112e
David <david.garcia@lumation.co>
2021-03-04 18:17:57 -0600

    Se importaron librerias y se trabajo con los middlewares

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 'use strict'
 
+//Global var
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+//Routes imports
+const user_routes = require('./src/routes/user.routes.js');
+
+//Middlewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Headboard
+app.use(cors());
+
+//Carga de rutas
+app.use('/api', user_routes);
 
 //Exports
 module.exports = app;
