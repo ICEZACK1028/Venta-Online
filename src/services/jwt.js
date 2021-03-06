2a9c2d01d7700be3fd332ac08fd1a94f1956d771
David <david.garcia@lumation.co>
2021-03-06 17:33:16 -0600

    Se creo el archivo jwt para el token

---
+++ src/services/jwt.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//Imports
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'cangreburguer';
+
+exports.createToken = function (user){
+    var payload = {
+        sub: user._id,
+        user: user.user,
+        rol: user.rol,
+        iat: moment().unix(),
+        exp: moment().day(10, 'days').unix()
+    }
+
+    return jwt.encode(payload, secret);
+
+}
