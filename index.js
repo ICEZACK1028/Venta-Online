dcf9bef9384b8d747748c775dd2d90ee1b909bda
David <david.garcia@lumation.co>
2021-03-07 00:09:36 -0600

    Se agrego la funciÃon para crear la categoria por defecto

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
 const Users = require('./src/models/user.model');
+const Categorys = require('./src/models/category.model');
 const app = require('./app');
 
 mongoose.Promise = global.Promise;
@@ -36,6 +37,31 @@ mongoose.connect('mongodb://localhost:27017/VentaOnline', { useNewUrlParser: tru
 
         }
     })
+
+    var Category = new Categorys();
+    var name = 'default';
+    var description = '';
+
+    Category.name = name;
+    Category.description = description;
+
+    Categorys.find({ name: Category.name }).exec((er, categoryFound)=>{
+        if(er) return res.status(500).send({ mensaje: '¡Error!' });
+
+        if(categoryFound && categoryFound.length >=1){
+            return console.log( 'Categorys already exist');
+        }else{
+            Category.save((er, categorySaved)=>{
+                if(er) return res.status(500).send({ mensaje: 'Ha ocurrido un error'});
+
+                if(categorySaved){
+                    return console.log({ categorySaved });
+                }else{
+                    return console.log({ mensaje: 'No se ha podido crear la categoria ' });
+                }
+            });
+        }
+    });
 
     app.listen(3000, () => console.log('Server ready port: 3000... '));
     
