e42e61476e4d6b2aa0fd5433928b98a3d332a744
David <david.garcia@lumation.co>
2021-03-07 00:08:52 -0600

    Se agregaron las rutas de categorias

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 
 //Routes imports
 const user_routes = require('./src/routes/user.routes.js');
+const category_routes = require('./src/routes/category.routes');
 
 //Middlewares
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,7 +18,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //Carga de rutas
-app.use('/api', user_routes);
+app.use('/api', user_routes, category_routes);
 
 //Exports
 module.exports = app;
