08a442e072ceeaf5d83125ab307322c5a3225388
David <david.garcia@lumation.co>
2021-03-07 00:14:53 -0600

    Se creo el modelo de categorias

---
+++ src/models/category.model.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CategorySchema = Schema({
+    name: String,
+    description: String
+});
+
+module.exports = mongoose.model('categorys', CategorySchema );
