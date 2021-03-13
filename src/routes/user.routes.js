3190d8ded51c004bb8251c5b20bc5687d95a62d5
David <david.garcia@lumation.co>
2021-03-12 19:49:36 -0600

    00

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,7 +13,13 @@ api.post('/registryUser', userController.registryUser);
 api.post('/login', userController.login);
 api.post('/createUser', md_autentication.ensureAuth,  userController.createUser);
 api.put('/updateRol/:idUser', md_autentication.ensureAuth, userController.updateRol);
-api.put('/editUser/:idUser', md_autentication.ensureAuth,  userController.editUser);
-api.delete('/deleteUser/:idUser', md_autentication.ensureAuth, userController.deleteUserClient);
+api.delete('/deleteUserClient/:idUser', md_autentication.ensureAuth, userController.deleteUserClient);
+api.put('/editUserClient/:idUser', md_autentication.ensureAuth,  userController.editUserClient);
+api.put('/editUser', md_autentication.ensureAuth,  userController.editUser);
+api.delete('/deleteUser', md_autentication.ensureAuth,  userController.deleteUser);
+api.get('/searchProductsName', md_autentication.ensureAuth, userController.searchProductsName);
+api.get('/searchCategory', md_autentication.ensureAuth, userController.searchCategory);
+api.get('/searchProductsCategory', md_autentication.ensureAuth, userController.searchProductsCategory);
+api.get('/mostSoldProducts', md_autentication.ensureAuth, userController.mostSoldProducts);
 
 module.exports = api;
